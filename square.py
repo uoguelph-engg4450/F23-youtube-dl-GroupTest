4403854969c2a42ce026bf584bbff9b5a2766189
pariya_ab <abadeh.pariya@gmail.com>
2023-10-21 22:22:56 -0400

    test for diff file

--- square.py
+++ square.py
@@ -1,6 +1,7 @@
 """ Squares the input. """
 import sys
-if __name__=="__main__":
- assert(len(sys.argv)==2)
- num = int(sys.argv[0])
- print num ** 2
+
+if __name__ == "__main__":
+    assert (len(sys.argv) == 1)
+    num = int(sys.argv[0])
+    print(num ** 2)
